073369c20972053345e57031478c192eff858e8c
sfulham <sfulham@shaunfulham.com.au>
2020-10-08 17:19:10 +1030

    Create Core.h, Delete MyClass

--- src/Engine/MyClass.cpp
+++
@@ -1,6 +0,0 @@
-#include "Engine/MyClass.h"
-
-MyClass::MyClass( int value )
-	: value_( value )
-{
-}
--- src/Engine/Utility/Log.cpp
+++ src/Engine/Utility/Log.cpp
@@ -1,5 +1,6 @@
 #include <ctime>
 #include <string>
+#include <cstring>
 #include <stdio.h>
 #include <stdarg.h>
 #include <filesystem>
@@ -15,107 +16,107 @@
 namespace gp1
 {
 
-bool fileLoggingEnabled = true;
-std::string logFilepath = "log.txt";
+    bool fileLoggingEnabled = false;
+    std::string logFilepath = "log.txt";
 
-// TODO(fkp): This currently opens the file each time we log. We
-// should instead probably save to a buffer first and flush it every
-// so often.
-void log(Severity severity, const char* format, ...)
-{
-	va_list args;
-	va_start(args, format);
+    // TODO(fkp): This currently opens the file each time we log. We
+    // should instead probably save to a buffer first and flush it every
+    // so often.
+    void log(Severity severity, const char* format, ...)
+    {
+        va_list args;
+        va_start(args, format);
 
-	FILE* file = nullptr;
+        FILE* file = nullptr;
 
-	if (fileLoggingEnabled)
-	{
-		std::filesystem::path filepath { logFilepath };
-		std::filesystem::create_directories(filepath.parent_path());
+        if (fileLoggingEnabled)
+        {
+            std::filesystem::path filepath { logFilepath };
+            std::filesystem::create_directories(filepath.parent_path());
 
-		file = fopen(logFilepath.c_str(), "a");
+            file = fopen(logFilepath.c_str(), "a");
 
-		if (file)
-		{
-			static bool hasDone = false;
+            if (file)
+            {
+                static bool hasDone = false;
 
-			if (!hasDone)
-			{
-				hasDone = true;
-				fprintf(file, "\n\n================================================================\n\n");
-			}
-		}
-		else
-		{
-			fileLoggingEnabled = false;
-			log(Severity::Warning, "Failed to open log file '%s' (error: %s).", logFilepath.c_str(), std::strerror(errno));
-		}
-	}
-	
-	constexpr unsigned int timeBufferSize = 16;
-	std::time_t currentTime = std::time(nullptr);
-	char timeBuffer[timeBufferSize];
-	
-	if (std::strftime(timeBuffer, timeBufferSize, "[%H:%M:%S]", std::localtime(&currentTime)))
-	{
-		printf("%s ", timeBuffer);
+                if (!hasDone)
+                {
+                    hasDone = true;
+                    fprintf(file, "\n\n================================================================\n\n");
+                }
+            }
+            else
+            {
+                fileLoggingEnabled = false;
+                log(Severity::Warning, "Failed to open log file '%s' (error: %s).", logFilepath.c_str(), std::strerror(errno));
+            }
+        }
 
-		if (file)
-		{
-			fprintf(file, "%s ", timeBuffer);
-		}
-	}
-	
-	switch (severity)
-	{
-	case Severity::Trace:
-		printf(WHITE "Trace" DEFAULT_COLOUR ": ");
+        constexpr unsigned int timeBufferSize = 16;
+        std::time_t currentTime = std::time(nullptr);
+        char timeBuffer[timeBufferSize];
 
-		if (file)
-		{
-			fprintf(file, "Trace: ");
-		}
-		break;
+        if (std::strftime(timeBuffer, timeBufferSize, "[%H:%M:%S]", std::localtime(&currentTime)))
+        {
+            printf("%s ", timeBuffer);
 
-	case Severity::Debug:
-		printf(BLUE "Debug" DEFAULT_COLOUR ": ");
-		if (file)
-		{
-			fprintf(file, "Debug: ");
-		}
-		break;
+            if (file)
+            {
+                fprintf(file, "%s ", timeBuffer);
+            }
+        }
 
-	case Severity::Warning:
-		printf(YELLOW "Warning" DEFAULT_COLOUR ": ");
-		if (file)
-		{
-			fprintf(file, "Warning: ");
-		}
-		break;
+        switch (severity)
+        {
+            case Severity::Trace:
+                printf(WHITE "Trace" DEFAULT_COLOUR ": ");
 
-	case Severity::Error:
-		printf(RED "Error" DEFAULT_COLOUR ": ");
-		
-		if (file)
-		{
-			fprintf(file, "Error: ");
-		}
-		break;
-	}
-	
-	vprintf(format, args);
-	printf("\n");
+                if (file)
+                {
+                    fprintf(file, "Trace: ");
+                }
+                break;
 
-	if (file)
-	{
-		vfprintf(file, format, args);
-		fprintf(file, "\n");
+            case Severity::Debug:
+                printf(BLUE "Debug" DEFAULT_COLOUR ": ");
+                if (file)
+                {
+                    fprintf(file, "Debug: ");
+                }
+                break;
 
-		fclose(file);
-	}
-	
-	va_end(args);
-}
+            case Severity::Warning:
+                printf(YELLOW "Warning" DEFAULT_COLOUR ": ");
+                if (file)
+                {
+                    fprintf(file, "Warning: ");
+                }
+                break;
+
+            case Severity::Error:
+                printf(RED "Error" DEFAULT_COLOUR ": ");
+
+                if (file)
+                {
+                    fprintf(file, "Error: ");
+                }
+                break;
+        }
+
+        vprintf(format, args);
+        printf("\n");
+
+        if (file)
+        {
+            vfprintf(file, format, args);
+            fprintf(file, "\n");
+
+            fclose(file);
+        }
+
+        va_end(args);
+    }
 
 }
 
