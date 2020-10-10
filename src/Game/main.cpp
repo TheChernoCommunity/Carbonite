c1fc2ad4088e54013b8eb5d348a7789bfa5b267c
Ben Cooper <bccooper1996@gmail.com>
2020-10-09 18:38:03 -0700

    Test using AssetLoader

--- src/Game/main.cpp
+++ src/Game/main.cpp
@@ -1,4 +1,5 @@
 #include "Engine/Utility/Log.h"
+#include "Engine/Utility/AssetLoader.h"
 
 #include <iostream>
 
@@ -8,6 +9,18 @@ int main( int /*argc*/, char* /*argv*/[] )
 	gp1::log(gp1::Severity::Debug, "Another string: '%s'", "Hello, World!");
 	gp1::log(gp1::Severity::Warning, "Oh no!");
 	gp1::log(gp1::Severity::Error, "Code: %d.", 1);
+
+	gp1::AssetLoader& assetLoader = gp1::AssetLoader::Get();
+
+	gp1::AssetLoadResult fooResult = assetLoader.LoadAsset("foo.txt");
+	if (fooResult.success)
+	{
+		gp1::log(gp1::Severity::Trace, "Successfully loaded foo.txt! Contents: %s", std::string(fooResult.data).c_str());
+	}
+	else
+	{
+		gp1::log(gp1::Severity::Warning, "Failed to load foo.txt");
+	}
 
 	return 0;
 }
