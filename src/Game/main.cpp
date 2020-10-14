8141338b5ced7d7f9fea88a72b4aed6acc5d4056
Sahil Iyer <iyersahil12@gmail.com>
2020-10-14 15:12:22 +1100

    Added GLFW and test window creation.

--- src/Game/main.cpp
+++ src/Game/main.cpp
@@ -1,13 +1,67 @@
 #include "Engine/Utility/Log.h"
 
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main( int /*argc*/, char* /*argv*/[] )
 {
-	gp1::log(gp1::Severity::Trace, "This is a trace message.");
-	gp1::log(gp1::Severity::Debug, "Another string: '%s'", "Hello, World!");
-	gp1::log(gp1::Severity::Warning, "Oh no!");
-	gp1::log(gp1::Severity::Error, "Code: %d.", 1);
+	if (!glfwInit())
+	{
+		gp1::log(gp1::Severity::Error, "Failed to initialise GLFW.");
+		return 1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(960, 540, "GameProject-1", nullptr, nullptr);
+
+	if (!window)
+	{
+		gp1::log(gp1::Severity::Error, "Failed to create window.");
+		glfwTerminate();
+		return 1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	float red = 0.0f;
+	float redInc = 0.001f;
+	float green = 1.0f;
+	float greenInc = -0.001f;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(red, green, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		red += redInc;
+		green += greenInc;
+
+		if (red <= 0.0f)
+		{
+			redInc *= -1;
+			red = 0.0f;
+		}
+
+		if (red >= 1.0f)
+		{
+			redInc *= -1;
+			red = 1.0f;
+		}
+
+		if (green <= 0.0f)
+		{
+			greenInc *= -1;
+			green = 0.0f;
+		}
+
+		if (green >= 1.0f)
+		{
+			greenInc *= -1;
+			green = 1.0f;
+		}
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
 	return 0;
 }
