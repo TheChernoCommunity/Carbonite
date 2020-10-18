be227fc34d8cfe9d0e26e9a786582800c87c9b7f
Shaun Fulham <sfulham@shaunfulham.com.au>
2020-10-18 19:08:56 +1030

    Start on Maths

---
+++ include/Engine/Utility/Maths/Vec.h
@@ -0,0 +1,77 @@
+#pragma once
+
+namespace gp1
+{
+	template<size_t S, typename T>
+	struct Vec;
+
+	template<typename T>
+	struct Vec<1, T>
+	{
+		union
+		{
+			struct
+			{
+				T x;
+			};
+
+			struct
+			{
+				T r;
+			};
+		};
+	};
+
+	template<typename T>
+	struct Vec<2, T>
+	{
+		union
+		{
+			struct
+			{
+				T x, y;
+			};
+
+			struct
+			{
+				T r, g;
+			};
+		};
+	};
+
+	template<typename T>
+	struct Vec<3, T>
+	{
+		union
+		{
+			struct
+			{
+				T x, y, z;
+			};
+
+			struct
+			{
+				T r, g, b;
+			};
+		};
+	};
+
+	template<typename T>
+	struct Vec<4, T>
+	{
+		union
+		{
+			struct
+			{
+				T x, y, z, w;
+			};
+
+			struct
+			{
+				T r, g, b, a;
+			};
+		};
+	};
+
+
+}
