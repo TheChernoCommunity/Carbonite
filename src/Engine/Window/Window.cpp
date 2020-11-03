8a4e2b7a18699f0e4e729b332ffe493cb2a135c0
Vironacorus <63048206+Vironacorus@users.noreply.github.com>
2020-11-03 19:33:46 +0100

    Textures and stuff

--- src/Engine/Window/Window.cpp
+++ src/Engine/Window/Window.cpp
@@ -33,7 +33,7 @@ namespace gp1
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_FALSE);
 
 		GLFWmonitor* monitor{ glfwGetPrimaryMonitor() };
--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -6,7 +6,7 @@
 #include "Engine/Application.h"
 #include "Engine/Events/KeyboardEvent.h"
 #include "Engine/Events/MouseEvent.h"
-#include "Engine/Renderer/DebugRenderer.h"
+#include "Engine/Graphics/Textures/TextureGL.h"
 
 namespace gp1 {
 
@@ -19,52 +19,58 @@ namespace gp1 {
 
 		// First get or create a new InputGroup with InputHandler::GetOrCreateInputGroup(name):
 		input::InputGroup* onFoot = input::InputHandler::GetOrCreateInputGroup("onFoot");
-		onFoot->SetCaptureMouse(true);
 		// Second create either a ButtonInputBinding or AxisInputBinding with their respective create functions:
-		input::ButtonInputBinding* openMenu = onFoot->CreateButtonInputBinding("openMenu", input::buttons::keyEscape);
+		input::ButtonInputBinding* openMenu = onFoot->CreateButtonInputBinding("openMenu", 256);
 		// Thirdly if you didn't set the binding in the create function you can do it like this:
 		openMenu->BindCallback(std::bind(&Game::OpenMenuCallback, this, std::placeholders::_1));
 		// The callback is void(gp1::input::ButtonCallbackData data) or void(gp1::input::AxisCallbackData data) depending on what type of binding.
 		// For an example setup look at the callbacks in this class.
-		m_Logger.LogDebug("OpenMenu keybind is: %u, on device: %u", openMenu->GetIndex(), static_cast<uint32_t>(openMenu->GetLocation()));
+		m_Logger.LogDebug("OpenMenu keybind is: %u, on device: %u", openMenu->GetIndex(), (uint32_t)openMenu->GetLocation());
 
-		input::AxisInputBinding* lookX = onFoot->CreateAxisInputBinding("lookX", input::axises::mouseX);
+		input::AxisInputBinding* lookX = onFoot->CreateAxisInputBinding("lookX", 0);
 		lookX->BindCallback(std::bind(&Game::LookCallback, this, std::placeholders::_1));
-		m_Logger.LogDebug("LookX keybind is: %u, on device: %u", lookX->GetIndex(), static_cast<uint32_t>(lookX->GetLocation()));
+		m_Logger.LogDebug("LookX keybind is: %u, on device: %u", lookX->GetIndex(), (uint32_t)lookX->GetLocation());
 
-		input::AxisInputBinding* lookY = onFoot->CreateAxisInputBinding("lookY", input::axises::mouseY);
+		input::AxisInputBinding* lookY = onFoot->CreateAxisInputBinding("lookY", 1);
 		lookY->BindCallback(std::bind(&Game::LookCallback, this, std::placeholders::_1));
-		m_Logger.LogDebug("LookY keybind is: %u, on device: %u", lookY->GetIndex(), static_cast<uint32_t>(lookY->GetLocation()));
+		m_Logger.LogDebug("LookY keybind is: %u, on device: %u", lookY->GetIndex(), (uint32_t)lookY->GetLocation());
 
 		input::InputGroup* inMenu = input::InputHandler::GetOrCreateInputGroup("inMenu");
-		input::ButtonInputBinding* closeMenu = inMenu->CreateButtonInputBinding("closeMenu", input::buttons::keyEscape);
+		input::ButtonInputBinding* closeMenu = inMenu->CreateButtonInputBinding("closeMenu", 256);
 		closeMenu->BindCallback(std::bind(&Game::CloseMenuCallback, this, std::placeholders::_1));
-		m_Logger.LogDebug("CloseMenu keybind is: %u, on device: %u", closeMenu->GetIndex(), static_cast<uint32_t>(closeMenu->GetLocation()));
+		m_Logger.LogDebug("CloseMenu keybind is: %u, on device: %u", closeMenu->GetIndex(), (uint32_t)closeMenu->GetLocation());
 
-		//DebugRenderer::DebugDrawPoint({ 0, 0, -2 }, 20.0f);
-		//DebugRenderer::DebugDrawSphere({ 0, 0, -2 }, 0.5f, 10.0f);
-		//DebugRenderer::DebugDrawLine({ -2, 1, -4 }, { 2, -1, -4 }, 10.0f);
-		//DebugRenderer::DebugDrawBox({ 0, 0, -3 }, { 1, 1, 1 }, { 0, 0, 0 }, 10.0f);
-		DebugRenderer::DebugDrawABox({ 0, -2, -7 }, 10.0f);
-		DebugRenderer::DebugDrawABox({ -1, -2, -7 }, 10.0f);
-		DebugRenderer::DebugDrawABox({ -2, -2, -7 }, 10.0f);
+		input::InputHandler::HandleEvent(KeyPressedEvent(256, false));
+		input::InputHandler::HandleEvent(MouseMovedEvent(100, 50));
+		input::InputHandler::HandleEvent(KeyPressedEvent(256, false));
+		input::InputHandler::HandleEvent(MouseMovedEvent(50, 100));
+
+		using namespace GL;
+		Texture t = Texture2DCreate({20,20});
+		GL::BindTexture(t,0);
+		GL::BindTexture(t, 0);
+		GL::UnbindTexture(0);
+		DeleteTexture(t);
 	}
 
 	void Game::LookCallback(input::AxisCallbackData data) {
-		if (data.m_Id == "lookX") {
+		switch (data.m_Axis) {
+		case 0:
 			m_Logger.LogDebug("Moved mouse X to %f", data.m_Value);
-		} else if (data.m_Id == "lookY") {
+			break;
+		case 1:
 			m_Logger.LogDebug("Moved mouse Y to %f", data.m_Value);
+			break;
 		}
 	}
 
 	void Game::OpenMenuCallback(input::ButtonCallbackData data) {
-		m_Logger.LogDebug("Opened Menu %u", (uint32_t)data.m_InputType);
+		m_Logger.LogDebug("Opened Menu");
 		input::InputHandler::SetCurrentActiveInputGroup("inMenu");
 	}
 
 	void Game::CloseMenuCallback(input::ButtonCallbackData data) {
-		m_Logger.LogDebug("Closed Menu %u", (uint32_t)data.m_InputType);
+		m_Logger.LogDebug("Closed Menu");
 		input::InputHandler::SetCurrentActiveInputGroup("onFoot");
 	}
 
