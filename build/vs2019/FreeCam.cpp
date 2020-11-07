19223efe505ed86621e1e3c13a676f928f092c04
Matthew <mh33188@gmail.com>
2020-11-07 17:55:58 +0000

    Fixed

--- build/vs2019/FreeCam.cpp
+++ build/vs2019/FreeCam.cpp
@@ -1,22 +1,33 @@
 #include "FreeCam.h"
 
+gp1::FreeCam::FreeCam()
+{
+		input::InputGroup* freecamGroup = input::InputHandler::GetOrCreateInputGroup("freecam");
+		freecamGroup->CreateAxisInputBinding("lookUp", 1)->BindCallback(std::bind(&FreeCam::LookUp, this, std::placeholders::_1));
+		freecamGroup->CreateAxisInputBinding("lookRight", 0)->BindCallback(std::bind(&FreeCam::LookRight, this, std::placeholders::_1));
+		freecamGroup->CreateButtonInputBinding("forward", GLFW_KEY_W)->BindCallback(std::bind(&FreeCam::MoveForward, this, std::placeholders::_1));
+		freecamGroup->CreateButtonInputBinding("backward", GLFW_KEY_S)->BindCallback(std::bind(&FreeCam::MoveBackward, this, std::placeholders::_1));
+		freecamGroup->CreateButtonInputBinding("left", GLFW_KEY_A)->BindCallback(std::bind(&FreeCam::MoveLeft, this, std::placeholders::_1));
+		freecamGroup->CreateButtonInputBinding("right", GLFW_KEY_D)->BindCallback(std::bind(&FreeCam::MoveRight, this, std::placeholders::_1));	
+}
+
 void gp1::FreeCam::LookUp(input::AxisCallbackData data)
 {
-	double ydiff = lastY - data.m_Value;
-	lastY = data.m_Value;
+	double ydiff = m_lastY - data.m_Value;
+	m_lastY = data.m_Value;
 
-	Pitch += ydiff * Sensitivity;
-	if (Pitch > 89.0f) Pitch = 89.0f;
-	if (Pitch < -89.0f) Pitch = -89.0f;
+	m_Pitch += ydiff * m_Sensitivity;
+	if (m_Pitch > 89.0f) m_Pitch = 89.0f;
+	if (m_Pitch < -89.0f) m_Pitch = -89.0f;
 
 	UpdateDir();
 }
 
 void gp1::FreeCam::LookRight(input::AxisCallbackData data)
 {
-	double xdiff = data.m_Value - lastX;
-	lastX = data.m_Value;
-	Yaw += xdiff;
+	double xdiff = data.m_Value - m_lastX;
+	m_lastX = data.m_Value;
+	m_Yaw += xdiff;
 
 	UpdateDir();
 }
@@ -24,33 +35,33 @@ void gp1::FreeCam::LookRight(input::AxisCallbackData data)
 void gp1::FreeCam::UpdateDir()
 {
 	glm::vec3 direction;
-	direction.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-	direction.y = sin(glm::radians(Pitch));
-	direction.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-	FrontDir = glm::normalize(direction);
+	direction.x = cos(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
+	direction.y = sin(glm::radians(m_Pitch));
+	direction.z = sin(glm::radians(m_Yaw)) * cos(glm::radians(Pitch));
+	m_FrontDir = glm::normalize(direction);
 }
 
 void gp1::FreeCam::MoveForward(input::ButtonCallbackData /*data*/)
 {
 	
-	CameraPos += FrontDir * CameraSpeed;
+	m_CameraPos += m_FrontDir * m_CameraSpeed;
 	UpdateView();
 }
 void gp1::FreeCam::MoveBackward(input::ButtonCallbackData /*data*/)
 {
-	CameraPos -= FrontDir * CameraSpeed;
+	m_CameraPos -= m_FrontDir * m_CameraSpeed;
 	UpdateView();
 }
 
 void gp1::FreeCam::MoveLeft(input::ButtonCallbackData /*data*/)
 {
-	CameraPos -= glm::normalize(glm::cross(FrontDir,UpDir)) * CameraSpeed;
+	m_CameraPos -= glm::normalize(glm::cross(m_FrontDir, m_UpDir)) * m_CameraSpeed;
 	UpdateView();
 }
 
 void gp1::FreeCam::MoveRight(input::ButtonCallbackData /*data*/)
 {
-	CameraPos += glm::normalize(glm::cross(FrontDir,UpDir)) * CameraSpeed;
+	m_CameraPos += glm::normalize(glm::cross(m_FrontDir, m_UpDir)) * m_CameraSpeed;
 	UpdateView();
 }
 
@@ -58,7 +69,7 @@ void gp1::FreeCam::MoveRight(input::ButtonCallbackData /*data*/)
 
 void gp1::FreeCam::UpdateView()
 {
-	ViewMatrix = glm::lookAt(CameraPos, CameraPos + FrontDir, UpDir);
+	m_ViewMatrix = glm::lookAt(m_CameraPos, m_CameraPos + m_FrontDir, m_UpDir);
 }
 
 
