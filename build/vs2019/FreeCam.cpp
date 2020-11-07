2189b0cd1fb934c49732eea39991050f9b793e3b
Matthew <mh33188@gmail.com>
2020-11-07 18:00:41 +0000

    Fixed pt2

--- build/vs2019/FreeCam.cpp
+++ build/vs2019/FreeCam.cpp
@@ -3,12 +3,12 @@
 gp1::FreeCam::FreeCam()
 {
 		input::InputGroup* freecamGroup = input::InputHandler::GetOrCreateInputGroup("freecam");
-		freecamGroup->CreateAxisInputBinding("lookUp", 1)->BindCallback(std::bind(&FreeCam::LookUp, this, std::placeholders::_1));
-		freecamGroup->CreateAxisInputBinding("lookRight", 0)->BindCallback(std::bind(&FreeCam::LookRight, this, std::placeholders::_1));
-		freecamGroup->CreateButtonInputBinding("forward", GLFW_KEY_W)->BindCallback(std::bind(&FreeCam::MoveForward, this, std::placeholders::_1));
-		freecamGroup->CreateButtonInputBinding("backward", GLFW_KEY_S)->BindCallback(std::bind(&FreeCam::MoveBackward, this, std::placeholders::_1));
-		freecamGroup->CreateButtonInputBinding("left", GLFW_KEY_A)->BindCallback(std::bind(&FreeCam::MoveLeft, this, std::placeholders::_1));
-		freecamGroup->CreateButtonInputBinding("right", GLFW_KEY_D)->BindCallback(std::bind(&FreeCam::MoveRight, this, std::placeholders::_1));	
+		freecamGroup->CreateAxisInputBinding("lookUp", input::axises::mouseY)->BindCallback(std::bind(&FreeCam::LookUp, this, std::placeholders::_1));
+		freecamGroup->CreateAxisInputBinding("lookRight", input::axises::mouseX)->BindCallback(std::bind(&FreeCam::LookRight, this, std::placeholders::_1));
+		freecamGroup->CreateButtonInputBinding("forward", input::buttons::keyW)->BindCallback(std::bind(&FreeCam::MoveForward, this, std::placeholders::_1));
+		freecamGroup->CreateButtonInputBinding("backward", input::buttons::keyS)->BindCallback(std::bind(&FreeCam::MoveBackward, this, std::placeholders::_1));
+		freecamGroup->CreateButtonInputBinding("left", input::buttons::keyA)->BindCallback(std::bind(&FreeCam::MoveLeft, this, std::placeholders::_1));
+		freecamGroup->CreateButtonInputBinding("right", input::buttons::keyD)->BindCallback(std::bind(&FreeCam::MoveRight, this, std::placeholders::_1));
 }
 
 void gp1::FreeCam::LookUp(input::AxisCallbackData data)
@@ -37,7 +37,7 @@ void gp1::FreeCam::UpdateDir()
 	glm::vec3 direction;
 	direction.x = cos(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
 	direction.y = sin(glm::radians(m_Pitch));
-	direction.z = sin(glm::radians(m_Yaw)) * cos(glm::radians(Pitch));
+	direction.z = sin(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
 	m_FrontDir = glm::normalize(direction);
 }
 
