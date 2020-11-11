d23e23e2650c708b4b109bb39b86d83ac32f486b
Sausty45 <36990678+Sausty45@users.noreply.github.com>
2020-11-11 21:43:55 +0100

    MINIMP3 and DR_LIBS are now git submodules

---
+++ src/Engine/Audio/AudioImplementations.cpp
@@ -0,0 +1,12 @@
+//
+//  Created by Sausty on Nov. 11. 2020
+//
+//  Description:
+//	Implementation of DR_WAV and MINIMP3.
+
+#define DR_WAV_IMPLEMENTATION
+#include "dr_wav.h"
+
+#define MINIMP3_IMPLEMENTATION
+#include "minimp3.h"
+#include "minimp3_ex.h"
